ac18ea8bfb7de9e2a11318e2c489830ed33ffae2
LynieV <vmcclimon33@yahoo.com>
2019-04-03 19:37:52 -0500

    update tables and save show file

--- app-name/db/migrate/20190227212753_create_clients_table.rb
+++ app-name/db/migrate/20190227212753_create_clients_table.rb
@@ -1,10 +1,12 @@
 class CreateClientsTable < ActiveRecord::Migration
   def change
     create_table :clients do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :date
       t.string :content
       t.integer :user_id
-
-     
+      t.timestamps null: false     
     end
   end
 end
