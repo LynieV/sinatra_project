0396f21ea14dfc3e196f047fbf1ea258b31311c9
LynieV <vmcclimon33@yahoo.com>
2019-05-07 13:33:32 -0500

    adjust spacing and add preferences column

--- app-name/db/migrate/20190227212753_create_clients_table.rb
+++ app-name/db/migrate/20190227212753_create_clients_table.rb
@@ -4,8 +4,10 @@ class CreateClientsTable < ActiveRecord::Migration
       t.string :first_name
       t.string :last_name
       t.string :date
+      t.string :preferences
       t.string :content
       t.integer :user_id
+
       t.timestamps null: false     
     end
   end
