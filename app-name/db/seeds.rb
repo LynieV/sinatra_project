dad23e7eedb897477cdfa0d69d2d721bbcf6db82
LynieV <vmcclimon33@yahoo.com>
2019-05-07 13:47:12 -0500

    update seed file to include preferences

--- app-name/db/seeds.rb
+++ app-name/db/seeds.rb
@@ -7,8 +7,8 @@ patrick = User.create(name: "Patrick", email: "patrick@patrick.com", password: "
 
 #create clients
 
-Client.create(first_name: "George", last_name: "Brown", date: "3/20", content: "Prefers hot table, start face up", user_id: lynie.id)
-Client.create(first_name: "Joey", last_name: "Smith", date: "3/18", content: "Prefers no heat, finish on face and scalp", user_id: lynie.id)
+Client.create(first_name: "George", last_name: "Brown", date: "3/20", preferences: "Prefers hot table, start face up", content: "FBM, focus neck and shoulders", user_id: lynie.id)
+Client.create(first_name: "Joey", last_name: "Smith", date: "3/18", preferences: "Prefers no heat, finish on face and scalp", content: "Upper body only, focus low back", user_id: lynie.id)
 
-Client.create(first_name: "Jane", last_name: "Plain", date: "3/15", content: "Prefers medium heat, start face down", user_id: patrick.id)
-Client.create(first_name: "Barbara", last_name: "Moore", date: "3/16", content: "Prefers hot table, T-pillow, finish feet", user_id: patrick.id)
+Client.create(first_name: "Jane", last_name: "Plain", date: "3/15", preferences: "Prefers medium heat, start face down", content: "FBM, relaxation", user_id: patrick.id)
+Client.create(first_name: "Barbara", last_name: "Moore", date: "3/16", preferences: "Prefers hot table, T-pillow, finish feet", content: "FBM, focus legs and feet", user_id: patrick.id)
