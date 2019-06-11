f9bca20bcca6b1bb30ee3b5482f90b6a930e8a79
LynieV <vmcclimon33@yahoo.com>
2019-06-11 16:04:32 -0500

    start new content in clients_controller

--- therapist-app/app/controllers/clients_controller.rb
+++ therapist-app/app/controllers/clients_controller.rb
@@ -1,3 +1,12 @@
 class ClientsController < ApplicationController
+    #get /new to render form to create new client 
+    get '/clients/new' do
+        erb :'/clients/new'
+    end
 
+    #post clients for create new client info
+
+    #show page for client
+
+    #index for clients
 end
