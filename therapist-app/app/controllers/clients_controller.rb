5d0bddbc5d38dfa0297f4f977eccc422a69ba19a
LynieV <vmcclimon33@yahoo.com>
2019-06-11 16:47:55 -0500

    update clients controller

--- therapist-app/app/controllers/clients_controller.rb
+++ therapist-app/app/controllers/clients_controller.rb
@@ -5,8 +5,26 @@ class ClientsController < ApplicationController
     end
 
     #post clients for create new client info
+    post '/clients' do
+        # create new and save, only save if there is content and user is logged in
+        if !logged_in?
+            redirect '/'
+        end
+
+        if params[:content] != "" #add all required fields
+            #create new entry
+            @client = Client.create(content: params[:content], user_id: current_user.id)
+            #add all params
+            redirect "/clients/#{@client.id}"
+        else
+            redirect '/clients/new'
+        end
+    end
 
     #show page for client
+    get '/clients/:id' do
+        @client = Client.find(params[:id])
+    end
 
     #index for clients
 end
